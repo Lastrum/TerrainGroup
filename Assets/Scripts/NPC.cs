51ec5606662029e27f93e4e31a4fdae0fb9f5c12
Lastrum <50644237+Lastrum@users.noreply.github.com>
2021-09-30 10:20:35 +0200

    sdsad

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC : MonoBehaviour
 {
+    public GameObject player;
+
+    private NavMeshAgent npc;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+        npc = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        npc.SetDestination(player.transform.position);
     }
 }
